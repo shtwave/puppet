9eb2a8e5c724ad4469b8da5cdaeffd0c25b5d529
SatchanP <ohkawak@kau.li>
2013-08-27 17:07:38 +0900

    add keepalved VIP module for Ubuntu/Debian

---
+++ keepalived_vip/manifests/init.pp
@@ -0,0 +1,26 @@
+#
+# keepalived module
+# 2013/08/27 @SatchanP
+#
+class keepalived_vip {
+
+    $packages = [
+        "ipvsadm",
+        "keepalived",
+    ]
+
+    define real_server($lvs_ip="") {
+        $filename = regsubst($name, ':', '')
+        file { "/etc/network/if-pre-up.d/${filename}":
+            mode    => 755,
+            owner   => "root",
+            group   => "root",
+            content => template("keepalived/etc/network/if-pre-up.d/ifcfg-lvs"),
+            notify  => Exec["${filename}"],
+        }
+        exec { "${filename}":
+            command => "/bin/bash /etc/network/if-pre-up.d/${filename}",
+        }
+    }
+}
+~                           
